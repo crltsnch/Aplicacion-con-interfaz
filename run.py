a461b9df383643738682e91c55248b01033bce67
crltsnch <91721777+crltsnch@users.noreply.github.com>
2023-02-13 15:32:38 +0000

    cambios para el data 2

---
+++ run.py
@@ -0,0 +1,9 @@
+from url import *
+
+
+urls1 = ["a.com", "b.com", "c.com", "d.com"]
+urls2 = ["a.com", "b.com", "c.com", "d.com", "e.com"]
+
+def menu():
+
+  print('[1] Ejecutar código de trabajo secuencial')
--- url.py
+++ url.py
@@ -3,6 +3,7 @@ from time import sleep
 from multiprocessing import Pool
 
 urls = ['a.com', 'b.com', 'c.com', 'd.com']
+urls2 = ["a.com", "b.com", "c.com", "d.com", "e.com"]
 
 def scrape(url):
   print('starting', url)
@@ -20,15 +21,19 @@ def secuencial(urls):
     result = scrape(url)
     output.append(result)
 
-def multiproceso(urls):
-  '''
-  Esta función trabaja con multiprocesos. Los 4 urls de nuestra lista se ejecutan a la vez.
-  '''
-  if __name__ == '__main__':  #Para que no se genere un bucle guardamos el codigo en un archivo y lo ejecutamos directamente
-    pool = Pool(processes = 4) # Contamos con 4 procesadores porque en nuestra lista de urls tenemos 4 elementos.
-    data = pool.map(scrape, urls) # pool.map llama a la función scrape con cada url de urls y la salida la guarda en la variable data.
-    pool.close()
-    print()
-    
-    for row in data:
-      print(row)  
+# El siguiente código trabaja con multiprocesos. Los 4 urls de nuestra lista se ejecutan a la vez.
+
+if __name__ == '__main__':  # Para que no se genere un bucle guardamos el codigo en un archivo y lo ejecutamos directamente
+  pool = Pool(processes = 4) # Contamos con 4 procesadores porque en nuestra lista de urls tenemos 4 elementos.
+  # pool.map llama a la función scrape con cada url de urls y la salida la guarda en la variable data.
+  data1 = pool.map(scrape, urls) # Aquí podemos observar   que los 4 elementos de la lista se ejecutan a la vez
+  data2 = pool.map(scrape, urls2) # Aquí vemos como al sólo tener 4 procesadores, el quinto elemento de nuestra lista no se ejecuta hasta que uno de los otros cuatro deje un 'puesto' libre.
+  pool.close()
+  print()
+  print('DATA 1')
+  for row in data1:
+    print(row)
+
+  print('DATA 2')
+  for row in data2:
+    print(row)
